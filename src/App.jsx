87843a84ce2565feedbaf53159b4dd67be820177
mattbtindall <matthewbtindall@gmail.com>
2024-05-08 06:46:17 +0100

    Add sass
    Also set up the ability to use camel case when importing the classes
    from modules (instead of kebab case)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import classes from './App.module.scss'
+
 function App() {
     return (
-        <h1>test</h1>
+        <>
+            <h1>test</h1>
+            <p className={classes.mainRed}>some text</p>
+        </>
     )
 }
 
